3d9efc8d9d31bed38945dfed3ba8c770b99eb7d8
Wayne-Jeong <ceojyh@nate.com>
2025-07-16 12:18:24 +0900

    script add from new2

---
+++ src/main/java/org/example/chap_01/_05_variableNaming.java
@@ -0,0 +1,27 @@
+package chap_01;
+
+import org.omg.CORBA.WStringSeqHelper;
+
+public class _05_variableNaming {
+    public static void main(String[] args) {
+        String nationality = "대한민국";  //국적
+        String firstName = "용훈";
+        String lastName = "wayne";
+        String dateOfBirth = "1982-05-19";
+        String residentialAddress = "노블레스 호텔";
+        String purposeOfVisit = "관광"; // 입국목적
+        String flightNo = "KE111";
+        String _flightNo = "KE111";
+        String flight_no_2 ="KE111";
+
+        int accompany = 2;
+        int lengthOfStay = 5;
+
+        String item1 = "시계";
+        String item2 = "가방";
+
+        final String CODE = "KR";
+
+
+    }
+}
---
+++ src/main/java/org/example/chap_01/_06_Constans.java
@@ -0,0 +1,15 @@
+package chap_01;
+
+public class _06_Constans {
+    public static void main(String[] args) {
+        final String KR_CONTURY_CODE = "+82";
+        //KR_CONTURY_CODE = "+82";
+        System.out.println(KR_CONTURY_CODE);
+
+        final double PI = 3.141592;
+        final String DATE_OF_BIRTH = "1982-05-15";
+
+
+
+    }
+}
